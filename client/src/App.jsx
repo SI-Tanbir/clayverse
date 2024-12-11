267e49dae7beb1f25acc13a10c8733dc163e1712
SI-tanbir <si017490@gmail.com>
2024-12-11 20:44:11 +0600

    finished the footer part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Pages/Footer";
 import Navbar from "./Pages/Navbar";
 import Slider from "./Pages/Slider";
 
@@ -9,6 +10,8 @@ function App() {
     
       <Navbar></Navbar>
       <Slider></Slider>
+
+      <Footer></Footer>
 
     </>
   );
